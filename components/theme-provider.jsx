04c5ede1336e7c4eeced25e927477f2db7ac8081
Piyush kumar <piyush2004.dev@gmail.com>
2025-02-15 22:07:13 +0530

    remove enableSystem attribute from NextThemesProvider

--- components/theme-provider.jsx
+++ components/theme-provider.jsx
@@ -5,12 +5,7 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({ children, ...props }) {
   return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="light"
-      enableSystem
-      {...props}
-    >
+    <NextThemesProvider attribute="class" defaultTheme="light" {...props}>
       {children}
     </NextThemesProvider>
   );
