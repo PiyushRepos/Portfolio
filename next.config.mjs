118d465b82b81ecb0993b2f4a5c13e10ae37a2ef
Piyush kumar <piyush2004.dev@gmail.com>
2025-02-15 21:39:05 +0530

    do some changes in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/Portfolio", // Change this to match your repository name
+  images: {
+    unoptimized: true, // Required for static exports
+  },
 };
 
 export default nextConfig;
