ee8f96a2f7269f9d504b4f47863d328448504f32
Piyush kumar <piyush2004.dev@gmail.com>
2025-02-15 20:27:50 +0530

    change defaultTheme to light

--- components/theme-provider.jsx
+++ components/theme-provider.jsx
@@ -7,7 +7,7 @@ export function ThemeProvider({ children, ...props }) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="light"
       enableSystem
       {...props}
     >
