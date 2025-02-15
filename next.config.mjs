22eb34e4ec7e77178b7475ca12ec05813859407c
Piyush kumar <piyush2004.dev@gmail.com>
2025-02-15 22:03:40 +0530

    add node env

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/Portfolio", // Change this to match your repository name
+  basePath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
   images: {
-    unoptimized: true, // Required for static exports
+    unoptimized: true,
   },
 };
 
